d9514ed4f2d8a58b2867c79c2a280ba449f19e28
Takayuki MATSUOKA <takayuki.matsuoka@gmail.com>
2018-05-21 01:08:00 +0900

    Add zlib 1.2.11

---
+++ Bz/zlib/zlib.c
@@ -0,0 +1,9 @@
+#define NO_GZIP
+
+#include "zlib-1.2.11/zlib.h"
+#include "zlib-1.2.11/adler32.c"
+#include "zlib-1.2.11/compress.c"
+#include "zlib-1.2.11/deflate.c"
+#include "zlib-1.2.11/trees.c"
+#include "zlib-1.2.11/uncompr.c"
+#include "zlib-1.2.11/zutil.c"
